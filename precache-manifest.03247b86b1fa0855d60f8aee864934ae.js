5d835bd507efcd9ab6212f9d943bacdefce223e2
Matthew Smith <matthew.k.smith@viasat.com>
2019-02-12 09:24:11 -0800

    Updates

---
+++ precache-manifest.03247b86b1fa0855d60f8aee864934ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b16784f5aabc2900b92",
+    "url": "/todo/static/js/runtime~main.3b16784f.js"
+  },
+  {
+    "revision": "2c86358f43353910a365",
+    "url": "/todo/static/js/main.2c86358f.chunk.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/todo/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "2c86358f43353910a365",
+    "url": "/todo/static/css/main.aa7bbc62.chunk.css"
+  },
+  {
+    "revision": "ebc0239e03affa02e3fe32cf7c4f08e8",
+    "url": "/todo/index.html"
+  }
+];
