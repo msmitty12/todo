3937ae5c76e94388a9604e73a7164c884818e7e7
Matthew Smith <matthewksmith12@gmail.com>
2019-02-18 17:27:50 -0800

    Updates

---
+++ precache-manifest.537978271e128498c391673b03791776.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94d1bffe5915b9a84070",
+    "url": "/todo/static/css/main.75dcb566.chunk.css"
+  },
+  {
+    "revision": "94d1bffe5915b9a84070",
+    "url": "/todo/static/js/main.94d1bffe.chunk.js"
+  },
+  {
+    "revision": "3b16784f5aabc2900b92",
+    "url": "/todo/static/js/runtime~main.3b16784f.js"
+  },
+  {
+    "revision": "cbcc5d4755167dbd1122",
+    "url": "/todo/static/js/2.cbcc5d47.chunk.js"
+  },
+  {
+    "revision": "664721df0356f3c69b6c3a2395e9ebec",
+    "url": "/todo/index.html"
+  }
+];
