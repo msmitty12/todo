02ede63a02a18f963b967b7dfcb575f6964002af
Matthew Smith <matthewksmith12@gmail.com>
2019-02-20 21:35:56 -0800

    Tasks get more red as they approach deadline

--- todo/src/components/Todos.js
+++ todo/src/components/Todos.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { markComplete, deleteTodo } from '../actions.js'
 
-class Todos extends Component {
-  render() {
-    const todosContainerStyle = {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      width: "100%"
+function getColor(remainingDays){
+    let lightness = "80"
+    const truncedDays = remainingDays > 4 ? 4 : remainingDays
+    const percent = (truncedDays / 4.0)
+    let hue=(percent*50).toString(10);
+    if (remainingDays < 0) {
+      lightness = "40"
+      hue = "0"
+    } else if (remainingDays > 4) {
+      hue = "50"
     }
+    return ["hsl(",hue,",100%,",lightness,"%)"].join("");
+}
+
+function datediff(first, second) {
+    // Take the difference between the dates and divide by milliseconds per day.
+    // Round to nearest whole number to deal with DST.
+    return Math.trunc((second-first)/(1000*60*60*24));
+}
+
+class Task extends Component {
+  render() {
     const itemStyle = {
       margin: "5px",
       backgroundColor: "lightyellow",
@@ -28,20 +42,37 @@ class Todos extends Component {
       borderRadius: "5px"
     }
 
+    let remainingDays = datediff(new Date(), new Date(this.props.dueDate))
+    const taskColor = getColor(remainingDays)
+
     return (
-      // TODO: Why is index bad? https://stackoverflow.com/questions/28329382/understanding-unique-keys-for-array-children-in-react-js
+      <div style={{...itemStyle, backgroundColor: taskColor}}>
+        <div style={nameStyle}>{this.props.name}</div>
+        <div style={descStyle}>{this.props.description}</div>
+        <div>Due Date: {this.props.dueDate}</div>
+        <div>{this.props.completed ? "Complete!" : "Incomplete"}</div>
+        <div>
+          <button onClick={() => this.props.dispatch(markComplete(this.props.name))} type="button">Mark Complete</button>
+          <button onClick={() => this.props.dispatch(deleteTodo(this.props.name))} type="button">Delete</button>
+        </div>
+      </div> 
+    )
+  }
+}
+
+class Todos extends Component {
+  render() {
+    const todosContainerStyle = {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      width: "100%"
+    }
+
+    return (
       <div style={todosContainerStyle}>
         {this.props.todos.sort(function(a, b){return new Date(a.dueDate) - new Date(b.dueDate)}).map((todo, index) => (
-            <div style={todo.completed ? {...itemStyle, backgroundColor: "lightgreen"} : itemStyle}>
-              <div style={nameStyle}>{todo.name}</div>
-              <div style={descStyle}>{todo.description}</div>
-              <div>Due Date: {todo.dueDate}</div>
-              <div>{todo.completed ? "Complete!" : "Incomplete"}</div>
-              <div>
-                <button onClick={() => this.props.dispatch(markComplete(todo.name))} type="button">Mark Complete</button>
-                <button onClick={() => this.props.dispatch(deleteTodo(todo.name))} type="button">Delete</button>
-              </div>
-            </div>
+            <Task {...todo} dispatch={this.props.dispatch} />
           ))}
       </div>
     )
