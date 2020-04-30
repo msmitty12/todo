9822d24330956a10cde8045260bb042630e017ea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-30 23:36:00 +0000

    Deploy msmitty12/todo to github.com/msmitty12/todo.git:gh-pages

---
+++ precache-manifest.1c40aa48f12b884443d73afae9b86e05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.3ce2d46c.js"
+  },
+  {
+    "revision": "9fecad75099789e62370",
+    "url": "/todo/static/js/main.9fecad75.chunk.js"
+  },
+  {
+    "revision": "a61e56b52c28b104c9a9",
+    "url": "/todo/static/js/2.a61e56b5.chunk.js"
+  },
+  {
+    "revision": "9fecad75099789e62370",
+    "url": "/todo/static/css/main.980f0186.chunk.css"
+  },
+  {
+    "revision": "a61e56b52c28b104c9a9",
+    "url": "/todo/static/css/2.78b9f850.chunk.css"
+  },
+  {
+    "revision": "36f69e1f2c07daea73be029f6ed71587",
+    "url": "/todo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/todo/precache-manifest.8141de615517005b8d9a80a698e0e9ab.js"
+  "/todo/precache-manifest.1c40aa48f12b884443d73afae9b86e05.js"
 );
 
 workbox.clientsClaim();
